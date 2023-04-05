2e7aa1668095c42ec0f188df5f2a59cf75e8cd80
Sergey Ko <u306060@gmail.com>
2023-04-04 19:14:32 -0600

    1.1.0-ttailwind

--- README.md
+++ README.md
@@ -3,30 +3,48 @@ OhSnap!.js
 
 > Oh Snap ! The server crashed... Try again later :D
 
-A simple jQuery/Zepto notification library designed to be used in mobile apps
+A simple jQuery/Zepto notification library designed to be used in mobile apps by **[Justin Domingue](https://github.com/justindomingue)**
 
 Check out this [demo page](http://justindomingue.github.io/ohSnap/ "Demo")
 
 ## Installation
+-----
 
 Dependency: [jQuery](http://jquery.com) or [Zepto](http://zeptojs.com).
 
 Download ohSnap.js:
-- from [Github](https://raw.githubusercontent.com/justindomingue/ohSnap/master/ohsnap.js)
-- with Bower: `bower install oh-snap`.
+- from [Github](https://raw.githubusercontent.com/way5/ohSnap-tailwind/tailwind/ohsnap.js)
+- with Yarn: <code>yarn add oh-snap</code>
 
-Add a `div.ohsnap` which will contain the alerts :
-
-    <div id="ohsnap"></div>
-
-Usage
+## Usage
 -----
+
+Within your js:
+
+```js
+import {ohSnap, ohSnapX} from 'ohsnap.js';
+```
+
+Default options:
+
+```json
+{
+    'title': "",
+    'color': 'base',
+    'icon': 'bell',     // Font Awesome icon name without  "fa-" (ex: fa-bell -> bell)
+    'duration': 7000,   // time is ms
+    'container': 'body', // ex: '#ohsnap', '.ohsnap', etc
+    'fade-duration': 'fast'
+}
+```
 
 To call a notification, use `ohSnap(text, options)`. Examples :
 
-    ohSnap('Succefully created your account');
-    ohSnap('Oh Snap! I cannot process your card...', {color: 'red', icon: 'icon-alert'}); // color matches CSS class `.alert.color`
-    ohSnap('Yeeaahh! You are now registered.', {time: '2000'} // time is ms
+```js
+ohSnap('Succefully created your account');
+ohSnap('Oh Snap! I cannot process your card...', {color: 'red', icon: 'pizza-slice'});
+ohSnap('Yeeaahh! You are now registered.', {time: '2000'});
+```
 
 To remove a notification, use `ohSnapX()`.
 
@@ -34,44 +52,67 @@ Alerts are automatically bound to a click event (internally, `ohSnapX()` is call
 
 ### Styling
 
-Furthermore, ohSnap.js creates a div with classes `.alert .alert-color` so you will want to have something like :
+- **tailwind.conf.js**
 
-    /* ALERTS */
-    /* inspired by Twitter Bootstrap */
+```json
+...
+module.exports = {
+    ...
+    theme: {
+        colors: {
+            ...
+            "yellow": {
+                50: "#fffde7",
+                100: "#fff9c4",
+                200: "#fff59d",
+                300: "#fff176",
+                400: "#ffee58",
+                500: "#ffeb3b",
+                600: "#fdd835",
+                700: "#fbc02d",
+                800: "#f9a825",
+                900: "#f57f17",
+            },
+            "green": {
+                50: "#e8f5e9",
+                100: "#c8e6c9",
+                200: "#a5d6a7",
+                300: "#81c784",
+                400: "#66bb6a",
+                500: "#4caf50",
+                600: "#43a047",
+                700: "#388e3c",
+                800: "#2e7d32",
+                900: "#1b5e20",
+            },
+            "indigo": {
+                50: "#e8eaf6",
+                100: "#c5cae9",
+                200: "#9fa8da",
+                300: "#7986cb",
+                400: "#5c6bc0",
+                500: "#3f51b5",
+                600: "#3949ab",
+                700: "#303f9f",
+                800: "#283593",
+                900: "#1a237e",
+            },
+            "red": {
+                50: "#ffebee",
+                100: "#ffcdd2",
+                200: "#ef9a9a",
+                300: "#e57373",
+                400: "#ef5350",
+                500: "#f44336",
+                600: "#e53935",
+                700: "#d32f2f",
+                800: "#c62828",
+                900: "#b71c1c",
+            },
+            ...
+        }
+    }
+}
+```
 
-    .alert {
-      padding: 15px;
-      margin-bottom: 20px;
-      border: 1px solid #eed3d7;
-      border-radius: 4px;
-      position: absolute;
-      bottom: 0px;
-      right: 21px;
-      /* Each alert has its own width */
-      float: right;
-      clear: right;
-    }
-
-    .alert-red {
-      color: white;
-      background-color: #DA4453;
-    }
-    .alert-green {
-      color: white;
-      background-color: #37BC9B;
-    }
-    .alert-blue {
-      color: white;
-      background-color: #4A89DC;
-    }
-    .alert-yellow {
-      color: white;
-      background-color: #F6BB42;
-    }
-    .alert-orange {
-      color:white;
-      background-color: #E9573F;
-    }
-
-
-By **Justin Domingue**
+- See: [ohsnap.scss](./ohsnap.scss)
--- ohsnap.js
+++ ohsnap.js
@@ -1,83 +1,132 @@
 /**
- * == OhSnap!.js ==
- * A simple jQuery/Zepto notification library designed to be used in mobile apps
- *
- * author: Justin Domingue
- * date: september 18, 2015
- * version: 1.0.0
- * copyright - nice copyright over here
- */
-
-/* Shows a toast on the page
- * Params:
- *  text: text to show
- *  options: object that can override the following options
- *    color: alert will have class 'alert-color'. Default null
- *    icon: class of the icon to show before the alert. Default null
- *    duration: duration of the notification in ms. Default 5000ms
- *    container-id: id of the alert container. Default 'ohsnap'
- *    fade-duration: duration of the fade in/out of the alerts. Default 'fast'
+* == OhSnap!.js ==
+* A simple jQuery/Zepto notification library designed to be used in mobile apps
+*
+* author: Justin Domingue
+* date: september 18, 2015
+* version: 1.1.0
+* copyright: MIT License (MIT)
+#####################################################################################
+CHANGELOG:
+- 2023/01/04 - added title, optimized for tailwind
+#####################################################################################
 */
-function ohSnap(text, options) {
-  var defaultOptions = {
-    'color'       : null,     // color is  CSS class `alert-color`
-    'icon'        : null,     // class of the icon to show before the alert text
-    'duration'    : '5000',   // duration of the notification in ms
-    'container-id': 'ohsnap', // id of the alert container
-    'fade-duration': 'fast',  // duration of the fade in/out of the alerts. fast, slow or integer in ms
-  }
 
-  options = (typeof options == 'object') ? $.extend(defaultOptions, options) : defaultOptions;
-
-  var $container = $('#'+options['container-id']),
-    icon_markup = "",
-    color_markup = "";
-
-    if (options.icon) {
-        icon_markup = "<span class='" + options.icon + "'></span> ";
+/**
+ * Shows a toast on the page
+ *
+ * @param string text - text to show
+ * @param {
+*      title: alert title. Default: null
+*      color: alert will have class 'alert-color'. Default: null
+*      icon: class of the icon to show before the alert. Default: null
+*      duration: duration of the notification in ms. Default: 5000ms
+*      container: wrapper element for all the alerts. Example: #some-class, .a-class, etc. Default: body
+*      fade-duration: duration of the fade in/out of the alerts. Default: 'fast'
+* } options - object that can override the following options
+*
+*/
+export function ohSnap(text, options) {
+    var defaultOptions = {
+        'title': null,
+        'color': 'base',
+        'icon': null,
+        'duration': 7000,
+        'container': 'body',
+        'fade-duration': 'fast'
     }
 
+    options = (typeof options == 'object') ? $.extend(defaultOptions, options) : defaultOptions;
+
+    var $container = $(options['container']),
+        icon_markup = "",
+        color_markup = "",
+        title = "";
+
     if (options.color) {
-      color_markup = 'alert-' + options.color;
+        color_markup = 'ohsnap-' + options.color;
+        if (options.icon === null) {
+            switch (options.color) {
+                case 'error':
+                    options.icon = 'bug';
+                    break;
+                case 'warning':
+                    options.icon = 'triangle-exclamation';
+                    break;
+                case 'success':
+                    options.icon = 'thumbs-up';
+                    break;
+                default:
+                    options.icon = 'bell';
+                    break;
+            }
+        }
+    }
+
+    if (options.icon) {
+        icon_markup = "<i class='fas fa-" + options.icon + "'></i> ";
+    }
+
+    if (options.title) {
+        title = '<h4 class="title">' + options.title + '</h4>';
     }
 
     // Generate the HTML
-    var html = $('<div class="alert ' + color_markup + '">' + icon_markup + text + '</div>').fadeIn(options['fade-duration']);
+    //   var html = $('<div class="alert ' + color_markup + '">' + icon_markup + text + '</div>').fadeIn(options['fade-duration']);
+    var html = $('<div class="ohsnap-wrapper">' +
+        '<div class="ohsnap-container ' + color_markup + '">' +
+        '<div class="content">' +
+        title +
+        '<p>' + text + '</p>' +
+        '</div>' +
+        '<div class="icon">' +
+        icon_markup +
+        '</div>' +
+        '</div>' +
+        '</div>'); //.fadeIn(options['fade-duration']);
 
     // Append the label to the container
     $container.append(html);
 
     // Remove the notification on click
-    html.on('click', function() {
+    html.on('click', function (e) {
         ohSnapX($(this));
     });
 
     // After 'duration' seconds, the animation fades out
-    setTimeout(function() {
-        ohSnapX(html);
-    }, options.duration);
+    if (options.duration != 0) {
+        setTimeout(function () {
+            ohSnapX(html);
+        }, options.duration);
+    } else {
+        $('.ohsnap-wrapper').on('click', (e) => {
+            ohSnapX(html);
+        });
+    }
 }
 
-/* Removes a toast from the page
- * params:
- *    Called without arguments, the function removes all alerts
- *    element: a jQuery object to remove
- *    options:
- *      duration: duration of the alert fade out - 'fast', 'slow' or time in ms. Default 'fast'
- */
-function ohSnapX(element, options) {
-    defaultOptions = {
-      'duration': 'fast'
+/**
+* Removes a toast from the page
+* Called without arguments, the function removes all alerts
+*
+* @param string element - a jQuery object to remove
+* @param {
+*      duration: duration of the alert fade out - 'fast', 'slow' or time in ms. Default 'fast'
+* } options
+*/
+export function ohSnapX(element, options) {
+    var defaultOptions = {
+        'fade-duration': 'fast'
     }
 
     options = (typeof options == 'object') ? $.extend(defaultOptions, options) : defaultOptions;
 
     if (typeof element !== "undefined") {
-        element.fadeOut(options.duration, function() {
+        element.fadeOut(options['fade-duration'], function () {
             $(this).remove();
         });
     } else {
-        $('.alert').fadeOut(options.duration, function() {
+        $('.ohsnap-wrapper').fadeOut(options['fade-duration'], function () {
             $(this).remove();
         });
     }
